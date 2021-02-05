d6f726f26ae32eb04665f335313b01510395e1a4
RandomMcDumDum <71903478+RandomMcDumDum@users.noreply.github.com>
2021-02-05 15:24:05 -0600

    Changed <App /> from stateless to to class componenet. Setting states to keep track of the remaining ammount of time on as well as a flag to see if the timer is currently counting down or not. Created some hooks to manage an Interval timer, but still need working on implementing timer

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,11 +1,33 @@
+import React, { Component } from 'react';
+import Countdown from './Countdown.js'
+import Header from './Header.js'
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <p>TESTING!</p>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      totalTime: 2400000,
+      mode: 'FOCUS',
+      countingDown: true
+    }
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount(){
+  }
+
+  render(){
+    return(
+      <div>
+        <Header mode={this.state.mode}/>
+        <Countdown time={this.state.totalTime}/>
+      </div>
+    );
+  }
 }
 
 export default App;
