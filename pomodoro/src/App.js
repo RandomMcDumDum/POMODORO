154f332578eed0b048906a5229c07d920f6c58dc
RandomMcDumDum <71903478+RandomMcDumDum@users.noreply.github.com>
2021-02-06 12:15:01 -0600

    Finally able to set up a working interval. Had to bind(this) to the time(). Loop starts only if countngDown is true. Timer() runs the calculation after a second has passed and updates the state of App. Started Work on a button to handleClick to start and stop the counting. Need to figure out how to initiate the interval since componentMounted() only gets called once at the begginging. Trying to useEffect() hook, will work on that next.

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import Countdown from './Countdown.js'
 import Header from './Header.js'
 import './App.css';
@@ -8,26 +8,49 @@ class App extends Component{
     super(props)
 
     this.state = {
-      totalTime: 2400000,
+      currentTime: 2000,
       mode: 'FOCUS',
-      countingDown: true
+      countingDown: false
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+ handleClick() {
+    this.setState({countingDown: !this.state.countingDown});
+  }
+
+  timer(){
+    this.setState({currentTime: this.state.currentTime - 1000})
+    console.log('Another second has passed...')
+    if(this.state.currentTime < 0 ){
+      clearInterval(this.intervalID)
+      this.setState({currentTime: 0});
     }
   }
 
   componentDidMount() {
+      if(this.state.countingDown){
+        this.intervalID = setInterval(this.timer.bind(this), 1000)
+      }
+  
+  
   }
 
   componentWillUnmount(){
+    clearInterval()
   }
 
   render(){
     return(
       <div>
         <Header mode={this.state.mode}/>
-        <Countdown time={this.state.totalTime}/>
+        <Countdown time={this.state.currentTime}/>
+        <button onClick={this.handleClick}>{this.state.countingDown ? 'STOP' : 'START'}</button>
       </div>
     );
   }
 }
+
 
 export default App;
